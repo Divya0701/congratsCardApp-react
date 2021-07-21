a4b2478ed226a99ed4de2638ca5864d425954132
Divya Yarra <nagababulaxmi944@gmai.com>
2021-07-21 06:17:03 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div className="container">
+    <h1>Congratulations</h1>
+    <div className="miniContainer">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="miniHeading">Kiran V</h1>
+      <p className="para">vishnu institute of technologies</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
